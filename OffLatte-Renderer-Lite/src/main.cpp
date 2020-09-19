2a496721fcc143f796e5640e2cc0fa348e3c4443
Dya Adel <Dyaplus@gmail.com>
2020-09-19 16:41:25 +0200

    Provided a hittable abstract class for spheres and a hittable_list

--- OffLatte-Renderer-Lite/src/main.cpp
+++ OffLatte-Renderer-Lite/src/main.cpp
@@ -1,33 +1,33 @@
+#include "utility.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
 #include <iostream>
 #include "ray.h"
 #include "render_output.h"
 
 double hit_sphere(const point3& center, const double radius, const ray& r) {
     vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
     if (discriminant < 0) {
         return -1.0;
     }
     else {
-        return (-b - sqrt(discriminant)) / (2.0 * a); //First intersection of sphere and ray
+        return (-half_b - sqrt(discriminant)) / a; //First intersection of sphere and ray
     }
 }
 
-color ray_gradient_color(const ray& r) {
-    vec3 sphere_o(0, 0, -1);
-    auto t = hit_sphere(sphere_o, 0.5, r);
-    if (t >= 0)
-    {
-        auto n = unit_vector(r.at(t) - sphere_o);
-        //Normal Map Rendering
-        return 0.5 * color(n.x + 1, n.y + 1, n.z + 1);
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y + 1.0);
+    auto t = 0.5 * (unit_direction.y + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -39,6 +39,11 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    //World
+    hittable_list world;
+    world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
     auto viewport_height = 2.0;
@@ -60,7 +65,8 @@ int main() {
             auto u = double(i) / (double(image_width) - 1);
             auto v = double(j) / (double(image_height) - 1);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin); // a point on the near clipping plane minus the origin for direction
-            write_color(std::cout,ray_gradient_color(r));
+            color pixel_color = ray_color(r, world);
+            write_color(std::cout,pixel_color); //Write color in ppm format to cout
         }
     }
     std::cerr << "\nDone.\n";
